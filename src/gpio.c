aa177e5ffe8c1b56808cd0c7b9da2ef5a3edeebc
Kaylin Luck <kluck@luc.edu>
2021-02-23 15:49:51 -0600

    blinky finished

--- src/gpio.c
+++ src/gpio.c
@@ -2,17 +2,19 @@ unsigned int *gpset1 = 0xFE200020;
 unsigned int *gpsel4 = 0xFE200010;
 unsigned int *gpclr1 = 0xFE20002C;
 
-void led_intit(){
+void led_init(){
 	unsigned int mask_number = 0xFFFFFE3F;
 	*gpsel4 = *gpsel4 & mask_number;
 	*gpsel4 = *gpsel4 | (1 << 6);
 	return;
 }
 
+//pin to 1
 void led_on(){
 	*gpset1 = (1 << 10);
 }
 
+//pin to 0
 void led_off(){
 	*gpclr1 = (1 << 10);
 }
